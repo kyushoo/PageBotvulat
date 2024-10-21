31337f054f760bd385e248676f52eb36755a6bab
MeowTzyッ <149583086+yuichi192168@users.noreply.github.com>
2024-10-21 10:03:13 +0800

    Update help.js

--- ws3/commands/help.js
+++ ws3/commands/help.js
@@ -1,28 +1,28 @@
 module.exports = {
   description: "See available commands",
   async run({ api, send, admin }) {
-  const quick_replies = [];
-  api.commands.forEach((name) => {
-    quick_replies.push({
+    const quick_replies = [];
+    api.commands.forEach((name) => {
+      quick_replies.push({
         content_type: "text",
         title: api.prefix + name,
         payload: name.toUpperCase()
+      });
     });
-  });
     try {
-    send({
-      quick_replies,
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: `🤖 | These are the commands of Nyamer below.
+      send({
+        quick_replies,
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: `🤖 | These are the commands of Nyamero below.
 🔎 | Click every command to see the usage.`
+          }
         }
-      }
-    });
-    } catch(err){
-     return send(err.message || err);
+      });
+    } catch (err) {
+      return send(err.message || err);
     }
   }
 };
